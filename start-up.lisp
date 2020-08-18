e8ff0437ddf6ff6bef77cf4fe1b81536804373b1
R. Wayne Oldford <rwoldford@users.noreply.github.com>
2020-08-18 10:56:23 -0400

    Add files via upload

---
+++ start-up.lisp
@@ -0,0 +1,18 @@
+(in-package :quail)
+(load "athena:Borel-Sets:borel-special1.lisp")
+(load "athena:Borel-Sets:set-collections.lisp")
+(load "athena:Borel-Sets:borel-sets.lisp")
+(load "athena:Borel-Sets:borel-sets2.lisp")  ;;two around
+(load "athena:Borel-Sets:borel-functions.lisp") ;;switch these
+(load "athena:Borel-Sets:borel-special2.lisp")
+;;;
+(load "athena:Borel-Sets:borel-set-methods.lisp")
+(load "athena:Borel-Sets:memberp-methods.lisp")
+(load "athena:Borel-Sets:insidep-methods.lisp")
+(load "athena:Borel-Sets:copy-methods.lisp")
+(load "athena:Borel-Sets:disjoint-sets-p-methods.lisp")
+(load "athena:Borel-Sets:simplify-methods.lisp")
+(load "athena:Borel-Sets:set-union-methods.lisp")
+(load "athena:Borel-Sets:set-intersection-methods.lisp")
+(load "athena:Borel-Sets:set-complement-methods.lisp")
+
